2338d009767c3244f6ed3d89c42716099ead444e
zigur <massimorgon@gmail.com>
2018-08-30 20:14:27 +0100

    implemented async script

--- src/scripts/import-from-fanfiction-net.js
+++ src/scripts/import-from-fanfiction-net.js
@@ -1,11 +1,27 @@
-// const parseArgs = require('minimist');
 const { Story, DataAccessObject } = require('../api/fanfiction-net-api');
+const incrspace = require('@stdlib/math/utils/incrspace');
+const connectionParams = require('../../local.js').databaseConnection;
 
 const argvs = require('minimist')(process.argv.slice(2));
 
 const min = argvs.min || 0;
 const max = argvs.max || 1000;
 
-for (let i = min; i < max; i++) {
-    
+const range = incrspace(min, max);
+
+async function saveStory(storyId, storyDAO) {
+    if (!storyId) {
+        return false;
+    }
+    const story = Story(storyId);
+    await story.fetchData();
+    story.parseData();
+    await story.loadChapters();
+    await storyDAO.insertStory(story);
+    return true;
 }
+
+const dao = new DataAccessObject(connectionParams);
+Promise.all(range.map(id => saveStory(id, dao))).then(results => {
+    console.log(results);
+});
